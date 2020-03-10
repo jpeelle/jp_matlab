ee9717819570e3aa56b3ccc065a855266eaa70e0
Jonathan Peelle <peelle@gmail.com>
2020-03-10 12:10:02 -0500

    Fixed typo

--- jp_makesequences_wrapper.m
+++ jp_makesequences_wrapper.m
@@ -5,18 +5,20 @@
 % and "hard" (each number in the sequence chosen at random.
 
 
+baseDir = '/Users/peelle/Box/PeelleLab/research_projects/201909188_LSCfMRI/shared/materials/Experiments/LSC_Motor-Task_v01';
+
 digitList = [2:5];
-numSequences = 500;
+numSequences = 242;
 sequenceLength = 9;
-repeatProbability = .3;
+repeatProbability = .4;
 
 
 easySequences = jp_makesequences(digitList, numSequences, sequenceLength, repeatProbability);
-dlmwrite('~/easySequences.tsv', easySequences, '\t');
+dlmwrite(fullfile(baseDir, 'easySequences.csv'), easySequences, 'delimiter', '');
 
 
-repeatProbability = [];
+repeatProbability = 0;
 
 hardSequences = jp_makesequences(digitList, numSequences, sequenceLength, repeatProbability);
-dlmwrite('~/hardSequences.tsv', easySequences, '\t');
+dlmwrite(fullfile(baseDir, 'hardSequences.csv'), hardSequences, 'delimiter', '');
 
